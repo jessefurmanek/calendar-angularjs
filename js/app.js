69082749fb05f4528bae48c54bb150fe69d672f8
jessefurmanek <jesse.furmanek@gmail.com>
2015-03-02 11:14:25 -0600

    Add angular from exercise

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-var app = angular.module('CalendarApp', []);
+var app = angular.module('CalendarApp', ['ngRoute']);
+
+app.config(function ($routeProvider) { 
+  $routeProvider 
+    .when('/', { 
+      controller: 'DayController', 
+      templateUrl: 'views/day.html' 
+    }) 
+    .when('/:id', { 
+      controller: 'EventController', 
+      templateUrl: 'views/event.html' 
+    })     
+    .otherwise({ 
+      redirectTo: '/' 
+    }); 
+});
